8f43ecf91584bb69bfc9d020020d514b186f37d1
Zatt Wine <tomclick.org@gmail.com>
2021-06-05 18:38:11 +0630

    add OPTIONS method in cors problem solve

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   // http method
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   // headers
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
