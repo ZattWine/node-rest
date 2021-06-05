1a1d39a400d28b80b581669f2f96c6fb49d1b4c6
Zatt Wine <tomclick.org@gmail.com>
2021-06-06 01:23:20 +0630

    connect post and user

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,9 +4,10 @@ const path = require("path");
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
+const User = require("../models/user");
 
 /**
- * GET posts from server.
+ * Fetch posts.
  */
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -23,13 +24,11 @@ exports.getPosts = (req, res, next) => {
         .limit(perPage);
     })
     .then((posts) => {
-      res
-        .status(200)
-        .json({
-          message: "Fetched posts successfully.",
-          posts: posts,
-          totalItems: totalItems,
-        });
+      res.status(200).json({
+        message: "Fetched posts successfully.",
+        posts: posts,
+        totalItems: totalItems,
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
@@ -41,7 +40,7 @@ exports.getPosts = (req, res, next) => {
 };
 
 /**
- * POST a post to server.
+ * Create a post.
  */
 exports.createPost = (req, res, next) => {
   // req validation
@@ -61,23 +60,35 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: {
-      name: "Zayar",
-    },
+    creator: req.userId,
   });
 
   post
     .save()
     .then((result) => {
-      // console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("No user found.");
+        error.statusCode = 404;
+        throw error;
+      }
 
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       res.status(201).json({
         message: "Post created successfully.",
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
