1e02d8d5e31a0906157610fc0f6a08ee8b422be4
Zatt Wine <tomclick.org@gmail.com>
2021-06-05 20:13:37 +0630

    connect to mongodb server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
@@ -25,4 +26,13 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://zattwine:q1lm5aP1EQbh1GpV@cluster0.c5muj.mongodb.net/shop?authSource=admin&replicaSet=atlas-uueejk-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
+  .then((result) => {
+    console.log("CONNECTED!");
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
