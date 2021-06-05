75df02b46c61e4d3b2fec114327b422bcaf71c86
Zatt Wine <tomclick.org@gmail.com>
2021-06-05 22:38:01 +0630

    add a user model

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -54,6 +55,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 // Errors handling
 app.use((error, req, res, next) => {
