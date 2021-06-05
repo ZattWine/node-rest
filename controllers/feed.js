d885bb93f95237c5a144ae9aa86957112340cb26
Zatt Wine <tomclick.org@gmail.com>
2021-06-05 17:43:04 +0630

    add multi comment to controller method

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+/**
+ * GET posts from server.
+ */
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -6,5 +9,24 @@ exports.getPosts = (req, res, next) => {
         content: "This is the first post!",
       },
     ],
+  });
+};
+
+/**
+ * POST a post to server.
+ */
+exports.postPost = (req, res, next) => {
+  // create post in db
+
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.status(201).json({
+    message: "Post created successfully.",
+    posts: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
+    },
   });
 };
